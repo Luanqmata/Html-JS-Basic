cc15792a0a6e4f5a0121f0303d3da83338196028
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2024-03-07 17:10:12 -0300

    Update Valor Menor e Maior.js

--- Sem-3/Valor Menor e Maior.js
+++ Sem-3/Valor Menor e Maior.js
@@ -1,6 +1,6 @@
- // Lista de valores
+
     var lista = [55, 44];
-    // Variáveis globais para armazenar maior e menor valor
+  
     var menor = lista[0];
     var maior = lista[0];
 
@@ -8,17 +8,17 @@
         console.log('A lista: ' + lista);
 
         for (var i = 0; i < lista.length; i++) {
-            // Compara o valor com o menor e atualiza se necessário
+          
             if (lista[i] < menor) {
                 menor = lista[i];
             } else if (lista[i] > maior) {
-                // Compara o valor com o maior e atualiza se necessário
+                
                 maior = lista[i];
             }
         }
     }
 
-    // Chamando a função
+   
     RetornaMaiorMenor(lista);
 
     console.log('O valor maior é: ' + maior + ' Forma crescente: ' + menor + ',' + maior);
