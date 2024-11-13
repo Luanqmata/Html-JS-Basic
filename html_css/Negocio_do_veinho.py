53d98d1fe810e3d6863b1f9ae8946e4f27aa3ae3
Aprendendo C# / Banco de Dados <138558778+Luanqmata@users.noreply.github.com>
2024-11-13 12:52:45 -0300

    Add files via upload

---
+++ html_css/Negocio_do_veinho.py
@@ -0,0 +1,37 @@
+# Html---Inicio
+## isso é simplismente uma atividade 
+
+
+REV
+
+       REM = E de ENDEREÇOS / a UC (unidade de controle) direciona o endreço que é armazenado na REM.
+
+       RAM = Random acess memory = usuario.
+       
+       ULA = Responsavel por calculos matematicos,Os numeros sao armazenados em REGISTRADORES de USO GERAL(B e C , A e D). onde o resultado é chamado de A=ACUMULADOR .
+       
+       RI = Registrador de instrução| passa pela UC e é responsavel pela ORIENTAÇÂO da ULA (Unidade Logica Aritmética), dizendo se é pra soma ou diminuir .
+
+       ROM = BIOS, BOOT.
+       
+       Stake Pointer = Apontador de pilhas. antigamente usava-se o FIFO (Frist-in-Frist-out) ou seja é uma forma mais lenta de buscar uma biblioteca que esteja no final da pilha ocasionando lentidão.
+       e o LIFO (Last-in-Frist-out) sendo assim uma forma mais eficaz usado pelos processadores,sendo assim a biblioteca q estiver no final da pilha sera chamada pelo seu endereço assim sendo ocorrendo o LIFO .
+       																				       
+				  																		LIFO= ultimo a entrar primeiro a sair .
+	Placa: Slots.
+ 	Chipset: Soquete.
+  	
+	MEMORIAS:
+    inferior   SRAM : Memorias Volateis F-F(Flipflop) Acabou a luz perdeu tudo.
+    Superior   DRAM : Memoria Dinamica BEM-Menor que o F-F com mais capacidade, porém Necessita de REFRESCAMENTO do MP (micro processador).
+
+ 	Capacidade de memoria : C=N*m  (N=numero de localidades , m = bit por vez)
+  				
+      				k-bits = 1024*1.
+      				k-bytes = 1024*8.
+	  			M-bytes = 1024 * 1024 * 8
+      					   1k     1k
+	      _______________________________________________________________________________________________________________________________________________________________________
+
+       conversão base ²(binaria) para base 16 (hexadecimal) = separa os binarios em grupos de 4 , e transforme o binario em normal.
+	      
---
+++ html_css/REVISAO_HTML.py
@@ -0,0 +1,35 @@
+REV 
+                                        HTML
+      <!Doctype> = declarar documento 
+        <html lang="pt-br"> = abre o tipo de codigo html e o idioma dele 
+        <head> =  cabeça a parte de comandos.
+        <body> = corpo pag 
+        <meta charset="utf-8"> = tipo de caracters do teclado.
+        <link rel = "stylesheet" href ="nome_da_pasta_css"> = faz a linkagem de uma pasta css , design
+        <header> = cabeçalho ,parte superior
+        <main> = divisão principal 
+        <div> = divisao genericá
+        <img src = "nome_da_imagem.jpg"> = faz a linkagem da imagem pra dentro do html
+        <span> = para estilo especifico 
+        <section> = sessão tematica ou agrupamento
+        <form> = abrir um formulario para ser preenchido 
+        <label> = Rótulo no form
+        <input> = abre uma caixa de dialogo
+        <nav> = menu de navegação
+        <a> = ancoragem de pagina pra pagina 
+        <ol> = lista ordenada 1 ,2 ,3 
+        <ul> = lista não ordenada 
+        <li> = item da lista 
+        <class> = cria uma classe ( . = css)
+        <id> = cria um id ( # = css)
+        <footer> = roda pé 
+        <em> = italico 
+        <strong> = grifado
+        <h1> = titulo
+        <p> = paragrafo
+        <br> = pula linha 
+                                      CSS
+
+        .nome_class = style na class
+        #idetifi
+        
